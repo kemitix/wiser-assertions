0d987fb6fb55aae8a05c51fcddfa89970d57afe2
Paul Campbell <pcampbell@kemitix.net>
2019-01-09 00:03:24 +0000

    Bump kemitix depedencies and convert changelog to org-mode (#49)
    * Bump kemitix-checkstyle-ruleset from 4.1.1 to 5.2.0
    
    * Bump kemitix-maven-tiles from 0.9.0 to 2.0.0
    
    * [changelog] reformat in org-mode
    
    * [changelog] update for next-release
    
    * [jenkins] remove sonarcloud
    
    * Bump kemitix-checkstyle from 5.2.0 to 5.3.2
    
    * Bump kemitix-maven-tiles form 2.0.0 to 2.1.1
    
    * Suppress spotbugs warnings about softening checked exceptions
    
    * [changelog] adjust heading levels and update next release
    
    * Bump kemitix-maven-tiles from 2.1.1 to 2.1.2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -39,16 +39,6 @@ pipeline {
                 }
             }
         }
-        stage('SonarQube (published)') {
-            when { expression { isPublished(publicRepo) } }
-            steps {
-                withSonarQubeEnv('sonarqube') {
-                    withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                        sh "${mvn} org.sonarsource.scanner.maven:sonar-maven-plugin:3.4.0.905:sonar"
-                    }
-                }
-            }
-        }
         stage('Deploy (published release branch)') {
             when {
                 expression {
